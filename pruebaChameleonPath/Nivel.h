e6ca7e9b4b3ae97f42797ffb5d00fbc1d7af0972
David Lopez <dnlm92@me.com>
2013-11-04 10:39:04 -0600

    [ADD] Clase de Nivel y clase de Pared

---
+++ pruebaChameleonPath/Nivel.h
@@ -0,0 +1,18 @@
+//
+//  Nivel.h
+//  pruebaChameleonPath
+//
+//  Created by Dave on 11/3/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Nivel : CCLayer {
+    
+}
+
++(CCScene *) scene;
+
+@end
